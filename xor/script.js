b500f87c44334a8b4c25589d87a46c749b95d006
mamengchen <624718596@qq.com>
2020-03-07 21:43:26 +0800

    js

--- xor/script.js
+++ xor/script.js
@@ -1,13 +1,12 @@
-import * as tfvis from '@tensorflow/tfjs-vis';
 import * as tf from '@tensorflow/tfjs';
-import {getData} from './data.js';
-import { input } from '@tensorflow/tfjs';
+import * as tfvis from '@tensorflow/tfjs-vis';
+import { getData } from './data.js';
 
 window.onload = async () => {
     const data = getData(400);
-    
+
     tfvis.render.scatterplot(
-        {name: 'xor_data'},
+        { name: 'XOR 训练数据' },
         {
             values: [
                 data.filter(p => p.label === 1),
@@ -15,41 +14,35 @@ window.onload = async () => {
             ]
         }
     );
-    
-    const model = tf.sequential();
 
-    //添加全链接层 神经元个数为4, relu主要提供非线性激活函数 第二层的sigmord用来压缩数据的
+    const model = tf.sequential();
     model.add(tf.layers.dense({
         units: 4,
         inputShape: [2],
         activation: 'relu'
     }));
-
     model.add(tf.layers.dense({
         units: 1,
         activation: 'sigmoid'
     }));
-
-    // 设置损失函数和优化器
     model.compile({
         loss: tf.losses.logLoss,
         optimizer: tf.train.adam(0.1)
     });
 
-    const inputs = tf.tensor(data.map(p => [p.x,p.y]));
+    const inputs = tf.tensor(data.map(p => [p.x, p.y]));
     const labels = tf.tensor(data.map(p => p.label));
 
-    await model.fit(inputs, labels,{
-        epochs: 50,
-        batchSize: 100,
+    await model.fit(inputs, labels, {
+        epochs: 10,
         callbacks: tfvis.show.fitCallbacks(
-            {name: '训练过程'},
+            { name: '训练效果' },
             ['loss']
         )
     });
 
-    window.predict = async (form) => {
-        const pred = await model.predict(tf.tensor([[form.x.value * 1, form.y.value * 1]]));
-        alert('预测结果： '+pred.dataSync()[0]);
-    }
+    window.predict = (form) => {
+        const pred = model.predict(tf.tensor([[form.x.value * 1, form.y.value * 1]]));
+        alert(`预测结果：${pred.dataSync()[0]}`);
+    };
 };
